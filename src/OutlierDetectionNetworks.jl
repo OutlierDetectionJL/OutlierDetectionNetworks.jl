fb126748ed701fd002571e9cf2969240a8b39cf6
David Muhr <muhrdavid@gmail.com>
2021-09-04 08:07:56 +0200

    simplify exports

--- src/OutlierDetectionNetworks.jl
+++ src/OutlierDetectionNetworks.jl
@@ -3,13 +3,6 @@ module OutlierDetectionNetworks
     using OutlierDetectionInterface:SCORE_UNSUPERVISED, SCORE_SUPERVISED
     const OD = OutlierDetectionInterface
 
-    export AEDetector,
-           AEModel,
-           DSADDetector,
-           DSADModel,
-           ESADDetector,
-           ESADModel
-
     include("utils.jl")
     include("models/ae.jl")
     include("models/dsad.jl")
@@ -23,6 +16,7 @@ module OutlierDetectionNetworks
     ORG = "OutlierDetectionJL"
     UUID = "51249a0a-cb36-4849-8e04-30c7f8d311bb"
     for model in MODELS
+        @eval(export $model)
         OD.metadata_pkg(model, package_name=@__MODULE__, package_uuid=UUID,
                         package_url="https://github.com/$ORG/$(@__MODULE__).jl",
                         is_pure_julia=true, package_license="MIT", is_wrapper=false)
