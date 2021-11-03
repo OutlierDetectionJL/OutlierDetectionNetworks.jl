abefc4c278c7f056284474b7768c3d9b52af2131
David Muhr <muhrdavid@gmail.com>
2021-11-03 16:31:27 +0100

    use macros to define the data frontend and meta

--- src/OutlierDetectionNetworks.jl
+++ src/OutlierDetectionNetworks.jl
@@ -16,10 +16,8 @@ module OutlierDetectionNetworks
 
     for model in MODELS
         @eval begin
-            OD.metadata_pkg($model, package_name=string(@__MODULE__), package_uuid=$UUID,
-                            package_url="https://github.com/OutlierDetectionJL/$(@__MODULE__).jl",
-                            is_pure_julia=true, package_license="MIT", is_wrapper=false)
-            OD.load_path(::Type{$model}) = string($model)
+            OD.@default_frontend $model
+            OD.@default_metadata $model $UUID
             export $model
         end
     end
